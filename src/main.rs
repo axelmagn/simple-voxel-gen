0aee20a1e0229dcd862487649448f64d125eebf9
Axel Magnuson <axelmagn@gmail.com>
2015-09-13 17:41:14 -0600

    bugfix types and minor bugs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 mod volume;
 
 use volume::*;
@@ -5,13 +9,20 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let vg = CornerCube {
+    env_logger::init().unwrap();
+
+    info!("creating Volume Generator");
+    let vg = Box::new(CornerCube {
         bounds: VolumeBounds(256, 256, 256),
         corner: VolumeIdx(128, 128, 128),
         val: 0.8,
-    };
+    });
+    info!("creating Volume Cursor");
     let vc = VolumeCursor::new(vg);
-    let buffer: Vec<u8> = vc.map(|&x| (x * 255) as u8).collect();
-    let mut f = try!(File::create("out.dat"));
-    try!(f.write_all(buffer.as_slice()));
+    info!("Creating Volume Buffer");
+    let buffer: Vec<u8> = vc.map(|x| (x * 255.0_f64) as u8).collect();
+    info!("Opening File");
+    let mut f = File::create("out.dat").unwrap();
+    info!("Writing File");
+    f.write_all(&buffer[..]).unwrap();
 }
--- src/volume.rs
+++ src/volume.rs
@@ -1,13 +1,13 @@
-use std::fs::File;
-use std::io;
+#[derive(Clone, Copy)]
+pub struct VolumeBounds(pub usize, pub usize, pub usize);
 
-pub struct VolumeBounds(usize, usize, usize);
-pub struct VolumeIdx(usize, usize, usize);
+#[derive(Clone, Copy)]
+pub struct VolumeIdx(pub usize, pub usize, pub usize);
 
 
 /// A Volume 
 pub trait Volume<T> where T: Sized {
-    fn get_bounds(&self) -> VolumeBounds;
+    fn get_bounds(&self) -> &VolumeBounds;
 
     /// Return Some(T) for an index, or None if it's out of bounds
     fn get(&self, idx: VolumeIdx) -> Option<T>;
@@ -15,13 +15,13 @@ pub trait Volume<T> where T: Sized {
 
 
 pub struct VolumeCursor<'a, T> where T: Sized {
-    volume: &'a (Volume<T> + 'static),
+    volume: Box<Volume<T> + 'a>,
     cursor: VolumeIdx,
 }
 
 
 impl<'a, T> VolumeCursor<'a, T> where T: Sized {
-    pub fn new(volume: &'a Volume<T>) -> VolumeCursor<'a, T> {
+    pub fn new(volume: Box<Volume<T> + 'a>) -> VolumeCursor<'a, T> {
         VolumeCursor {
             volume: volume,
             cursor: VolumeIdx(0, 0, 0),
@@ -34,8 +34,10 @@ impl<'a, T> Iterator for VolumeCursor<'a, T> {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        let VolumeBounds(bx, by, bz) = self.volume.get_bounds();
+        let VolumeBounds(bx, by, bz) = *self.volume.get_bounds();
+        let mut out = self.volume.get(self.cursor.clone());
         let VolumeIdx(mut cx, mut cy, mut cz) = self.cursor;
+        trace!("VolumeCursor.next {:?}", (cx, cy, cz));
 
         cx += 1;
         if cx >= bx {
@@ -47,25 +49,26 @@ impl<'a, T> Iterator for VolumeCursor<'a, T> {
             cz += 1;
         }
         if cz >= bz {
-            return None;
+            out = None;
         }
 
-        self.get(VolumeIdx(cx, cy, cz))
+        self.cursor = VolumeIdx(cx, cy, cz);
+        out
     }
 }
 
 
 /// A simple volume generator for testing
 pub struct CornerCube {
-    bounds: VolumeBounds,
-    corner: VolumeIdx,
-    val: f64,
+    pub bounds: VolumeBounds,
+    pub corner: VolumeIdx,
+    pub val: f64,
 }
 
 
 impl Volume<f64> for CornerCube {
-    fn get_bounds(&self) -> VolumeBounds {
-        self.bounds
+    fn get_bounds(&self) -> &VolumeBounds {
+        &self.bounds
     }
 
     fn get(&self, idx: VolumeIdx) -> Option<f64> {
